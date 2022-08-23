2ae4b9a6a7608182178e8c48906ab7b2a8b943ae
Shadowrunner11 <matematicadivina1@gmail.com>
2022-08-23 08:13:45 -0500

    :bug: port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import server from './services';
-// import 'dotenv/config';
+import 'dotenv/config';
 
-const PORT = Number(process.env);
+const { PORT } = process.env;
 
-server.listen(PORT,'0.0.0.0', ()=>{
+server.listen(Number(PORT),'0.0.0.0', ()=>{
   // eslint-disable-next-line no-console
   console.log('Listening at http://localhost:'+PORT);
 });
