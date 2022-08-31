220a86c406739995a9ee3ade597d1ff0bfd1208d
Shadowrunner11 <matematicadivina1@gmail.com>
2022-08-31 06:05:55 -0500

    deploy

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,14 +3,31 @@ import { Router } from 'express';
 
 import userController from '../controllers/user.controller';
 import { jwtCreate } from '../utils/jwtCreate';
+import cors from 'cors';
+
+// import process from 'process';
+
+//TODO colocar todo esto de las envs en configuraciono un YML mejor
+// const { FRONT_END_URL: origin = /http:\/\/localhost:5173.*/ } = process.env;
+const origin = /https:\/\/shadowrunner11.github.io\/tzuzulCodeReact.*/;
 const userApp = Router();
+
+const corsOptions = {
+  origin,
+  optionsSuccessStatus: 200,
+  preflightContinue: true,
+  allowedHeaders: 'X-Requested-With,content-type',
+  credentials: true
+};
+
+userApp.use(cors(corsOptions));
 
 userApp.options('/login', (_, res) => {
   /*  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', 'true'); */
-  res.sendStatus(200);
+  res.sendStatus(200).json({ success: true });
 });
 
 userApp.options('/register', (_, res) => {
@@ -18,7 +35,7 @@ userApp.options('/register', (_, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', 'true'); */
-  res.sendStatus(200);
+  res.sendStatus(200).json({ succes: true });
 });
 
 //TODO crear flujo co codigos de respuesta apropiados, temporalmente se envia payload
